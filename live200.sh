e2a53f78feaf890b94427aeedb17e0d55a01e937
JISS JOSE <55292980+jisjose98@users.noreply.github.com>
2021-12-25 20:17:32 +0530

    full code without help flags

---
+++ live200.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#write a script to sort live domains
+echo -e \n"
+      ██╗██╗███████╗███████╗    ███████╗██╗   ██╗██████╗ ███████╗███╗   ██╗██╗   ██╗███╗   ███╗███████╗██████╗ 
+      ██║██║██╔════╝██╔════╝    ██╔════╝██║   ██║██╔══██╗██╔════╝████╗  ██║██║   ██║████╗ ████║██╔════╝██╔══██╗
+      ██║██║███████╗███████╗    ███████╗██║   ██║██████╔╝█████╗  ██╔██╗ ██║██║   ██║██╔████╔██║█████╗  ██████╔╝
+ ██   ██║██║╚════██║╚════██║    ╚════██║██║   ██║██╔══██╗██╔══╝  ██║╚██╗██║██║   ██║██║╚██╔╝██║██╔══╝  ██╔══██╗
+ ╚█████╔╝██║███████║███████║    ███████║╚██████╔╝██████╔╝███████╗██║ ╚████║╚██████╔╝██║ ╚═╝ ██║███████╗██║  ██║
+  ╚═══╝ ╚═╝╚══════╝╚══════╝    ╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝"
+ 																		echo "Credits:Jiss Jose"
+}
+
+echo "The subdomain finder and sorts alive and removes dupes.Run by typing 
+
+git clone https://github.com/jisjose98/live200*
+sudo bash live200.sh
+
+
+#begins
+
+echo "live domain filterer"
+echo "Enter domain name"
+read dom
+echo -e "\n"
+subfinder -silent -d $dom -o subs.txt
+
+while read value; do
+	alive=$(cat subs.txt | httprobe | tee alive.txt);
+done < subs.txt
+echo "----------"
+subnum=$(cat alive.txt | wc -l)
+#sort dupes
+echo "Number of sudomains alive is $subnum"
+echo "****************sorted**********"
+
+sort alive.txt > sorted.txt
+sleep 3
+clear
+cat sorted.txt
